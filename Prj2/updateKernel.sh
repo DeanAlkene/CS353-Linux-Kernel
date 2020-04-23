b760148e35389d81ecdc076a590e221dd3fb91bd
DeanAlkene <deanlhz@sjtu.edu.cn>
2020-04-23 22:12:13 +0800

    Start Prj2

---
+++ Prj2/updateKernel.sh
@@ -0,0 +1,4 @@
+cp sched.h /usr/src/linux-5.6.6/include/linux/sched.h
+cp fork.c /usr/src/linux-5.6.6/kernel/fork.c
+cp core.c /usr/src/linux-5.6.6/kernel/sched/core.c
+cp base.c /usr/src/linux-5.6.6/fs/proc/base.c
